92e7f114a942b2d98b1bc167d7c8dc530888ef71
Sunbin <shen1qing9@163.com>
2020-10-27 08:32:41 +0000

    填充用户和话题数据

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -1,20 +1,24 @@
 <?php
 
-namespace App\Policies;
+use Faker\Generator as Faker;
 
-use App\Models\User;
-use App\Models\Topic;
+$factory->define(App\Models\Topic::class, function (Faker $faker) {
 
-class TopicPolicy extends Policy
-{
-    public function update(User $user, Topic $topic)
-    {
-        // return $topic->user_id == $user->id;
-        return true;
-    }
+    $sentence = $faker->sentence();
 
-    public function destroy(User $user, Topic $topic)
-    {
-        return true;
-    }
-}
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+
+    // 为创建时间传参，意为最大不超过 $updated_at，因为创建时间需永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
+    return [
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'user_id' => $faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        'category_id' => $faker->randomElement([1, 2, 3, 4]),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
+    ];
+});
