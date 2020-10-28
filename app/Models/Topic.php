ac7373df321ab0b192c13a1c97a312a04527ac27
Sunbin <shen1qing9@163.com>
2020-10-28 06:37:16 +0000

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -51,4 +51,9 @@ class Topic extends Model
     {
         return $this->hasMany(Reply::class);
     }
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
